62214451b9a7b2ad30dd437a6229da639f5931cc
Daniel morais <daniel.patrickmorais@gmail.com>
2026-03-17 21:58:08 -0300

    feat: implement user creation and retrieval functionality with updated controllers and model methods

--- src/controllers/user/createUserController.js
+++ src/controllers/user/createUserController.js
@@ -1,15 +1,20 @@
-import { createUser } from '../models/userModel.js';
+import { createUser } from "../../models/userModel.js"
 
 export async function createUserController(req, res) {
 
-    const user = {
-        avatar: "https://github.com/danielpmorais.png",
-        name: "Daniel Morais",
-        email: "daniel.patrickmorais@gmail.com",
-        pass: "12345678"
-    }
+    const user = req.body;
+
+    //     {
+    //   "name": "daniel",
+    //   "email": "daniel.morais@gmail.com",
+    //   "pass": "123456",
+    //   "avatar": "https://github.com/danielpmorais.png"
+    //     }
 
     const result = await createUser(user);
 
-    res.json({message: "Usuário criado com sucesso!", user: result});
+    res.json({
+        message: "Usuário criado com sucesso!",
+        user: result
+    });
 }
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,7 +1,11 @@
-import {prisma} from '../helpers/dbConnection.js';
+import { prisma } from '../helpers/dbConnection.js';
 
 export const createUser = async (user) => {
     return await prisma.user.create({
         data: user
     });
+}
+
+export const getUsers = async () => {
+    return await prisma.user.findMany();
 }
