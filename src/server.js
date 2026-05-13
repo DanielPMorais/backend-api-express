ac82e103cd765e902f689556b667813eec499cf3
Daniel morais <daniel.patrickmorais@gmail.com>
2026-05-12 21:27:58 -0300

    feat: add 404 error handling middleware to respond with a JSON message for non-existent routes

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 //const express = require('express')
-import express from 'express';
-import userRouter from './routers/userRouter.js';
-import publicationRouter from './routers/publicationRouter.js';
-import { logger } from './middlewares/logger.js'
-import { errorHandler } from './middlewares/errorHandler.js';
+import express from "express";
+import userRouter from "./routers/userRouter.js";
+import publicationRouter from "./routers/publicationRouter.js";
+import { logger } from "./middlewares/logger.js";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { error404 } from "./middlewares/error404.js";
 
 const app = express();
 const port = 3000;
@@ -12,14 +13,15 @@ app.use(logger);
 app.use(express.json());
 // req.body
 
-app.get('/', (req, res) => {
-  res.send('Hello');
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World Express API!" });
 });
 
-app.use('/user', userRouter);
-app.use('/publication', publicationRouter);
+app.use("/user", userRouter);
+app.use("/publication", publicationRouter);
 
-app.use(errorHandler)
+app.use(error404);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
